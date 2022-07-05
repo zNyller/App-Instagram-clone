905166ea7dcaf7ba842bc81a2751729ed35cb583
Nyller <edu.sampaio1777@gmail.com>
2022-07-05 13:22:57 -0300

    Versão com visualização dos dados do perfil

--- app/src/main/java/com/example/instagram/fragments/PerfilFragment.java
+++ app/src/main/java/com/example/instagram/fragments/PerfilFragment.java
@@ -11,14 +11,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.example.instagram.R;
 import com.example.instagram.activity.EditarPerfilActivity;
 import com.example.instagram.activity.PerfilAmigoActivity;
+import com.example.instagram.adapter.AdapterGrid;
 import com.example.instagram.helper.ConfigFirebase;
 import com.example.instagram.helper.UsuarioFirebase;
+import com.example.instagram.model.Postagem;
 import com.example.instagram.model.Usuario;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -26,6 +29,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -33,12 +42,16 @@ public class PerfilFragment extends Fragment {
     private Button buttonEditarPerfil;
     private TextView textPublicacoes, textSeguidores, textSeguindo;
     private CircleImageView imagePerfil;
+    private GridView gridViewPerfil;
 
     private DatabaseReference databaseRef;
     private DatabaseReference usuariosRef;
     private DatabaseReference usuarioAtualRef;
+    private DatabaseReference postagensUsuarioRef;
     private ValueEventListener valueEventListenerPerfil;
     private Usuario usuarioAtual;
+    private ImageLoader imageLoader = ImageLoader.getInstance();
+    private AdapterGrid adapterGrid;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -55,12 +68,17 @@ public class PerfilFragment extends Fragment {
         usuarioAtual = UsuarioFirebase.getDadosUsuarioAtual();
         databaseRef = FirebaseDatabase.getInstance().getReference();
         usuariosRef = databaseRef.child("usuarios");
+        // Configura referencia postagens usuario
+        postagensUsuarioRef = ConfigFirebase.getDatabase()
+                .child("postagens")
+                .child(usuarioAtual.getIdUsuario());
 
         buttonEditarPerfil = view.findViewById(R.id.buttonAcaoPerfil);
         textPublicacoes = view.findViewById(R.id.textPublicacoes);
         textSeguidores = view.findViewById(R.id.textSeguidores);
         textSeguindo = view.findViewById(R.id.textSeguindo);
         imagePerfil = view.findViewById(R.id.imagePerfil);
+        gridViewPerfil = view.findViewById(R.id.gridPerfil);
 
         buttonEditarPerfil.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,6 +96,11 @@ public class PerfilFragment extends Fragment {
                     .load(uri).into(imagePerfil);
         }
 
+        inicializarImageLoader();
+
+        // Carrega as fotos das postagens de um usuário
+        carregarPostagens();
+
         return view;
     }
 
@@ -91,6 +114,17 @@ public class PerfilFragment extends Fragment {
     public void onStop() {
         super.onStop();
         usuarioAtualRef.removeEventListener(valueEventListenerPerfil);
+    }
+
+    public void inicializarImageLoader(){
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration
+                .Builder(getActivity())
+                .memoryCache(new LruMemoryCache(2 * 1024 * 1024))
+                .memoryCacheSize(2 * 1024 * 1024)
+                .diskCacheSize(50 * 1024 * 1024)
+                .diskCacheFileCount(100)
+                .build();
+        imageLoader.init(config);
     }
 
     private void recuperarDadosPerfilUsuario() {
@@ -117,4 +151,36 @@ public class PerfilFragment extends Fragment {
             }
         });
     }
+
+    public void carregarPostagens(){
+        // Recupera as fotos postadas pelo usuario
+        postagensUsuarioRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // Configurar o tamanho do grid
+                int tamanhoGrid = getResources().getDisplayMetrics().widthPixels;
+                int tamanhoImagem = tamanhoGrid / 3;
+                gridViewPerfil.setColumnWidth(tamanhoImagem);
+
+                List<String> urlFotos = new ArrayList<>();
+                for (DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    Postagem postagem = dataSnapshot.getValue(Postagem.class);
+                    urlFotos.add(postagem.getCaminhoFoto());
+                }
+                int qtdPostagens = urlFotos.size();
+                textPublicacoes.setText(String.valueOf(qtdPostagens));
+
+                // Configurar adapter GridView
+                adapterGrid = new AdapterGrid(getActivity(), R.layout.grid_postagem, urlFotos);
+                gridViewPerfil.setAdapter(adapterGrid);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
 }
